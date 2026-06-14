949b306ced0e25942e616f16defdb01189e3c1ea
Youssef Mahmoud <youssefmahmoud2011@outlook.com>
2026-06-14 21:14:27 +0300

    Initial commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Scentz.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
